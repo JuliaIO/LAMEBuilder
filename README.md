38b4c081061f2f77b7dcd7b3d07fd0821d068c68
SimonDanisch <sdanisch@gmail.com>
2018-07-12 13:23:40 +0200

    Add autogenerated files to build liblame

---
+++ README.md
@@ -0,0 +1,9 @@
+# LAMEBuilder
+
+[![Build Status](https://travis-ci.org/SimonDanisch/LAMEBuilder.svg?branch=master)](https://travis-ci.org/SimonDanisch/LAMEBuilder)
+
+This repository builds binary artifacts for the liblame project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/SimonDanisch/LAMEBuilder/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,54 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "liblame"
+version = v"3.100.0"
+
+# Collection of sources required to build liblame
+sources = [
+    "https://downloads.sourceforge.net/lame/lame-3.100.tar.gz" =>
+    "ddfe36cab873794038ae2c1210557ad34857a4b6bdc515785d1da9e175b1da1e",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd lame-3.100/
+case $(uname -m) in    i?86) sed -i -e 's/<xmmintrin.h/&.nouse/' configure ;; esac
+./configure --prefix=$prefix --host=$target 
+make -j${ncore}
+make install
+exit
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:i686, :glibc),
+    Linux(:x86_64, :glibc),
+    Linux(:aarch64, :glibc),
+    Linux(:armv7l, :glibc, :eabihf),
+    Linux(:powerpc64le, :glibc),
+    Linux(:i686, :musl),
+    Linux(:x86_64, :musl),
+    Linux(:aarch64, :musl),
+    Linux(:armv7l, :musl, :eabihf),
+    FreeBSD(:x86_64)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libmp3lame", :libmp3lame)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    "https://github.com/SimonDanisch/NASMBuilder/releases/download/2.13.3/build_nasm.v2.13.3.jl"
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
